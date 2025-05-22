ea969141f07ce679a72e1134d1bc67f649c1399c
Caio Uehara <caioueharamartins@gmail.com>
2025-05-22 09:30:09 -0300

    Install three.js-react-fiber and add initial page.jsx

---
+++ app/(portal)/imageViewer/page.jsx
@@ -0,0 +1,34 @@
+"use client"
+
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+export default function Scene() {
+  return (
+    <Canvas
+      camera={{ position: [0, 0, 5], fov: 50 }}
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        backgroundImage: "url('/29-F-68.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+
+      {/* aqui só a caixa, sem precisar do BackgroundImage */}
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <OrbitControls />
+    </Canvas>
+  )
+}
